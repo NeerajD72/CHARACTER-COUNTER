8e76be3c046d8d3c4a468fd8002a53cb17ab2939
Neerak Kumar <neeraj@gmail.com>
2025-11-28 16:54:10 +0530

    All Files Uploaded

---
+++ script.js
@@ -0,0 +1,16 @@
+const text = document.getElementById('text')
+const character = document.getElementById('cha')
+const words = document.getElementById('word')
+const btn = document.getElementById('button')
+
+
+btn.addEventListener('click',()=>{
+  const text_field = text.value
+
+  const cha_count = text_field.length
+  character.textContent = cha_count
+
+  //trim function use for remove the space from the start and end
+  const word_count = text_field.trim().split(' ')
+  words.textContent = word_count.length
+})
